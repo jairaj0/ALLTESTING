f3b9a5fa1b7396d0580493015e3eb9b4a37ea09b
Jai Rawat <68697355+jairaj0@users.noreply.github.com>
2023-01-25 13:58:10 +0530

    chagne 1

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const sendGetRequest = async () => {
-try {
-    var myHeaders = new Headers({"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzQ2NzAyNzksInRva2VuIjoidjEwWVwvaWZrSnJXOXVCNElIM2ZaZWZwK0l6UjF5Qk5iMjd4RlR5V25xcGNYb013Z1wvZWRmVWR0bVdZZ0doTnJUbUlDcFVxMUdsS2pCalVNSk5LV3pqU3FIWkZaRGd1MTZwenAyS1doV1wvMldaanBSVXNsaVp3NUMwcHBzWG9YTkVrc2VtbHJWYVhFVUJ0VHlCWXBjWGNVMHNIUW9OdzNMa2VSN2xsdVBlbGI4VjhMazRpSkNMa2ZDY2x3QVkzb2VcL0VKTThwc1ZZRExHUWgrdWtVdnBDTTZGZ2ZjQWdRamlSbG01UHNKZExrNnVUWjBJNVpPaGZQNlg3VTU5WUdhRSt6c0FQdzBkY0txUmFVbVJ6TzNUV2VBPT0iLCJoaWRlWmVyb1N0YXR1cyI6dHJ1ZX0.rHsJW-R7tdAhFrZPSVvRx7nWgTUU3ERhBV3bLPpVHps",
-                                "Origin": "https://alltesting.vercel.app",
-                                 "Host": "https://demo.btrlexchange.com",
-                                });
-
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("https://demo.btrlexchange.com/api/global", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-} catch (error) {
-    error && console.log("error by client=>" , error)
-}
-  };
+    try {
+        var myHeaders = new Headers({"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzQ2NzAyNzksInRva2VuIjoidjEwWVwvaWZrSnJXOXVCNElIM2ZaZWZwK0l6UjF5Qk5iMjd4RlR5V25xcGNYb013Z1wvZWRmVWR0bVdZZ0doTnJUbUlDcFVxMUdsS2pCalVNSk5LV3pqU3FIWkZaRGd1MTZwenAyS1doV1wvMldaanBSVXNsaVp3NUMwcHBzWG9YTkVrc2VtbHJWYVhFVUJ0VHlCWXBjWGNVMHNIUW9OdzNMa2VSN2xsdVBlbGI4VjhMazRpSkNMa2ZDY2x3QVkzb2VcL0VKTThwc1ZZRExHUWgrdWtVdnBDTTZGZ2ZjQWdRamlSbG01UHNKZExrNnVUWjBJNVpPaGZQNlg3VTU5WUdhRSt6c0FQdzBkY0txUmFVbVJ6TzNUV2VBPT0iLCJoaWRlWmVyb1N0YXR1cyI6dHJ1ZX0.rHsJW-R7tdAhFrZPSVvRx7nWgTUU3ERhBV3bLPpVHps",
+                                    "Origin": "https://alltesting.vercel.app",
+                                     "Host": "https://demo.btrlexchange.com",
+                                     "Cache-Control" : "no-cache",
+                                     "Content-Type" : "application/x-www-form-urlencoded",
+                                     "Accept": "*/*"
+                                    });
+    
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    
+    fetch("https://demo.btrlexchange.com/api/global", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    } catch (error) {
+        error && console.log("error by client=>" , error)
+    }
+      };
 
 sendGetRequest()
